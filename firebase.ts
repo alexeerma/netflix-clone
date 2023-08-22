1271eba56374f1745dd6473ee9773e9c5468962c
alexeerma <aleksander.eerma@hotmail.com>
2023-08-22 14:06:20 +0300

    Initial commit

---
+++ firebase.ts
@@ -0,0 +1,8 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyBezbeeeZBESHkK8qzTExM7dhz0lCDwisw",
+    authDomain: "netflix-clone-d032d.firebaseapp.com",
+    projectId: "netflix-clone-d032d",
+    storageBucket: "netflix-clone-d032d.appspot.com",
+    messagingSenderId: "881927240908",
+    appId: "1:881927240908:web:e4b8c11906bef74b875669"
+  };
---
+++ src/components/index.ts
@@ -0,0 +1,13 @@
+import Navbar from './Navbar';
+
+
+
+
+
+
+
+
+
+export {
+    Navbar
+}
